91a51308f9e1299fdd97e50494891a3cbe4ff28e
Tom Brooks tombrooks248@gmail.com <tombrooks248@gmail.com>
2023-04-12 10:06:19 +0100

    tb 120423 notes now functional

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from note_storage import notes_list
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", notes_list =notes_list, enumerate=enumerate)
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
---
+++ note_storage.py
@@ -0,0 +1,7 @@
+notes_list =[
+    {"title": "example note title",
+     "content": "example note contet"},
+    {"title": "note two",
+     "content": "some more example note content"},
+
+]
